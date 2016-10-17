ac731f4480d9f02e5f4519b8f0d57a27f42b312a
Lokie Wang <lokie.wang@5imakeup.com>
2016-10-17 14:41:51 +0800

    使用Yeoman脚手架初始化工程

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('squadGoalFontEnd', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'toastr']);
+
+})();
